31ed7fcdc2441a849f505cda088e64cef04cff61
sriharan40 <sriharan40@gmail.com>
2016-10-06 10:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,30 @@
 'use strict';
 
 module.exports = require("./module/apiai");
+
+/*!
+ * apiai
+ * Copyright(c) 2015 http://api.ai/
+ * Apache 2.0 Licensed
+ */
+
+//var apiai = require("../module/apiai");
+var apiai = require("apiai");
+
+var app = apiai("c743619629b2490fab9751dac552094a");
+
+var options = {
+    sessionId: '<UNIQE SESSION ID>'
+}
+
+var request = app.textRequest('Hello', options);
+
+request.on('response', function(response) {
+    console.log(response);
+});
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
