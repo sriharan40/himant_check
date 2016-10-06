c2821be939d629230de59d32d1d49e2bf85b29d3
sriharan40 <sriharan40@gmail.com>
2016-10-06 08:09:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,23 @@
  * Apache 2.0 Licensed
  */
 
-'use strict';
+// var apiai = require("../module/apiai");
+var apiai = require("apiai");
 
-module.exports = require("./module/apiai");
+var app = apiai("c743619629b2490fab9751dac552094a");
+
+var options = {
+    sessionId: '<UNIQE SESSION ID>'
+}
+
+var request = app.textRequest('Hello', options);
+
+request.on('response', function(response) {
+    console.log(response);
+});
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
