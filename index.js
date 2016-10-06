b76946166f39a53c99ca9898026c9c9af1e97a3a
sriharan40 <sriharan40@gmail.com>
2016-10-06 12:34:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,23 +4,6 @@
  * Apache 2.0 Licensed
  */
 
-// var apiai = require("../module/apiai");
-var apiai = require("apiai");
+'use strict';
 
-var app = apiai("c743619629b2490fab9751dac552094a");
-
-var options = {
-    sessionId: '<UNIQE SESSION ID>'
-}
-
-var request = app.textRequest('Hello', options);
-
-request.on('response', function(response) {
-    console.log(response);
-});
-
-request.on('error', function(error) {
-    console.log(error);
-});
-
-request.end();
+module.exports = require("./module/apiai");
