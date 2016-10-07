bc456ee70c5e508746854f1e4408186c0c8cf057
sriharan40 <sriharan40@gmail.com>
2016-10-07 16:59:15 +0800

    Update newcheck.js

--- newcheck.js
+++ newcheck.js
@@ -12,21 +12,7 @@ var options = {
 }
 
 
-http.createServer(function(request, response) {
-  var resultp = app.textRequest();
-    console.log('resp is' + resultp.result.action);
-  request.on('error', function(err) {
-    console.error(err);
-  }).on('data', function(chunk) {
-    //body.push(chunk);
-  }).on('end', function() {
-    //body = Buffer.concat(body).toString();
-    // BEGINNING OF NEW STUFF
-
-    response.on('error', function(err) {
-      console.error(err);
-    });
-
+request.on('response', function(response) {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
     // Note: the 2 lines above could be replaced with this next one:
@@ -40,9 +26,22 @@ http.createServer(function(request, response) {
 
     response.write(JSON.stringify(responseBody));
     response.end();
-     // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
+	console.log(response);
+});
+	
+request.on('error', function(error) {
+    console.log(error);
+});
 
-    // END OF NEW STUFF
-  });
-}).listen((process.env.PORT), () => console.log("Server listening"));
+request.end();
+
+//}).listen((process.env.PORT), () => console.log("Server listening"));
+
+//}).listen(process.env.PORT);
+
+var server = http.createServer((request, response) => response.send(response));
+
+//Lets start our server
+server.listen((process.env.PORT), () => console.log("Server listening")); 
+var server = http.createServer((request, response) => response.send(response));
+
