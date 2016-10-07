27bed4a92587af4195e6106fbf9d540639a7274d
sriharan40 <sriharan40@gmail.com>
2016-10-08 01:32:53 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-/*!
- * apiai
- * Copyright(c) 2015 http://api.ai/
- * Apache 2.0 Licensed
- */
-
-//var apiai = require("../module/apiai");
-
+var http = require('http');
 var express = require('express');
-
 var router = express.Router();
-
-var bodyParser  = require('body-parser');
-
 var util = require("util");
-
 var apiai = require("apiai");
 
-var http = require('http');
-
 var app = apiai("c743619629b2490fab9751dac552094a");
-    
+var speech = 'We will send you an OTP now. Please check your mobile';
+
 var options = {
     sessionId: '<UNIQE SESSION ID>'
 }
 
-http.createServer(function(request, response){
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+	
+   request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+	   var textReq = app.textRequest();
+	   console.log (textReq['ClientRequest']);
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
 
-var headers = request.headers;
-var method = request.method;
-var url = request.url;
-var body = [];
-
-request.on('error', function(err) {
-console.error(err);
-}).on('data', function(chunk) {
-body.push(chunk);
-}).on('end', function() {
-body = Buffer.concat(body).toString();
+   response.on('error', function(err) {
+      console.error(err);
+    });
 
 //var name = request.param('customerName');
 
@@ -111,27 +103,24 @@ client.sms.messages.create({
         console.log('Oops! There was an error.');
     }
 });
-
-response.on('error', function(err) {
-  console.error(err);
-});
-
-response.statusCode = 200;
-
-response.setHeader('Content-Type', 'application/json');
-
-var speech = 'We will send you an OTP now. Please check your mobile';
+    
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+	   
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.writeHead(200, {'Content-Type': 'application/json'})
 
      var responseBody = {
         "speech": speech,
         "displayText": speech,
         "source": "apiai-Himant-OTP sample"
     };
+	  
+    response.write(JSON.stringify(responseBody));
+    response.end();
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.end(JSON.stringify(responseBody))
 
-response.write(JSON.stringify(responseBody));
-
-response.end();
-
-});
-  
+    // END OF NEW STUFF
+  });
 }).listen((process.env.PORT), () => console.log("Server listening"));
