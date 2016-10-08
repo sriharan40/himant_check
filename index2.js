d95d6f9182ad28550e16e93c2e0687fc8873bbb4
sriharan40 <sriharan40@gmail.com>
2016-10-08 19:10:08 +0800

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,29 @@
+var http = require('http')  
+var express = require('express');
+//var router = express.Router();
+var router = express();
+var util = require("util");
+var apiai = require("apiai");
+
+var app = apiai("c743619629b2490fab9751dac552094a");
+
+var options = {
+    sessionId: '<UNIQE SESSION ID>'
+}
+
+//const port = 3000
+
+const requestHandler = (request, response) => {  
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(process.env.PORT, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${process.env.PORT}`)
+})
