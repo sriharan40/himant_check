ec3e26e847fca8eb346c4f39220001d0636d33ad
sriharan40 <sriharan40@gmail.com>
2016-10-08 20:52:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,18 +19,15 @@ http.createServer(function(request, response) {
   request.on('error', function(err) {
     console.error(err);
   }).on('data', function(chunk) {
-	 //  var textReq = app.textRequest();
-	//   console.log (textReq['ClientRequest']);
-	   body += chunk;	  
-      // body.push(chunk);
-	   
+	body += chunk;	  
+        
 	   //console.log(body.customerName);
   }).on('end', function() {
     //body = Buffer.concat(body).toString();
 
 try {
       var data = JSON.parse(body);
-//	console.log(data);	
+	
     } catch(e) {
         console.log('malformed request', body);
         //  return response.status(400).send('malformed request: ' + body);
@@ -40,79 +37,62 @@ try {
       console.error(err);
     });
 
-//var name = request.param('customerName');
-
-//var mobile = request.param('phone-number');
-
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
-
- 
-//router.use(bodyParser.json());
-
-//router.use(bodyParser.urlencoded({
-//    extended: true
-//}));
-
-//router.post("/", function (req, res) {
-//    console.log(req.result.parameters.phone-number)
-//});
-
-//console.log(body);
+// PARSE THE BODY DATA AND THEN TAKE ACTIONS.
 
 var data = JSON.parse(body);
 
 var action = data.result.action;
+	  
+// TWILIO SMS
 if(action == "sendOTP")
 {
-var otp = Math.floor(1000 + Math.random() * 9000);
-var name = data.result.parameters.customerName;
-var mobile = data.result.parameters.phone;
-	  
-var speech = name + ', We will send you an OTP now. Please check your mobile';
+	var otp = Math.floor(1000 + Math.random() * 9000);
+	var name = data.result.parameters.customerName;
+	var mobile = data.result.parameters.phone;
 
-var value = name + mobile;
+	var speech = name + ', We will send you an OTP on your number' + mobile +'. Please reply back here with that OTP.';
 
-var request = app.textRequest(value, options);
+	var value = name + mobile;
 
-// Load the twilio module
+	var request = app.textRequest(value, options);
 
-// Twilio Credentials 
-var accountSid = 'ACe0b6cfbf60f11584099ee062db873252'; 
+	// Load the twilio module
 
-var authToken = '7468f40b17004327190847d04b4222ba'; 
+	// Twilio Credentials 
+	var accountSid = 'ACe0b6cfbf60f11584099ee062db873252'; 
 
-var client = require('twilio')(accountSid, authToken);
+	var authToken = '7468f40b17004327190847d04b4222ba'; 
 
-// Create a new REST API client to make authenticated requests against the
-// twilio back end
-//var client = new twilio.RestClient('ACe0b6cfbf60f11584099ee062db873252', '7468f40b17004327190847d04b4222ba');
+	var client = require('twilio')(accountSid, authToken);
 
-// Pass in parameters to the REST API using an object literal notation. The
-// REST client will handle authentication and response serialzation for you.
-client.sms.messages.create({
-    to: '+63'+mobile,
-    from: '+18312165009',
-    body: 'One time password for verification is' + otp
-}, function(error, message) {
-    // The HTTP request to Twilio will run asynchronously. This callback
-    // function will be called when a response is received from Twilio
-    // The "error" variable will contain error information, if any.
-    // If the request was successful, this value will be "falsy"
-    if (!error) {
-        // The second argument to the callback will contain the information
-        // sent back by Twilio for the request. In this case, it is the
-        // information about the text messsage you just sent:
-        console.log('Success! The SID for this SMS message is:');
-        console.log(message.sid);
- 
-        console.log('Message sent on:');
-        console.log(message.dateCreated);
-    } else {
-        console.log('Oops! There was an error.');
-    }
-});
+	// Create a new REST API client to make authenticated requests against the
+	// twilio back end
+	//var client = new twilio.RestClient('ACe0b6cfbf60f11584099ee062db873252', '7468f40b17004327190847d04b4222ba');
+
+	// Pass in parameters to the REST API using an object literal notation. The
+	// REST client will handle authentication and response serialzation for you.
+	client.sms.messages.create({
+	    to: '+63'+mobile,
+	    from: '+18312165009',
+	    body: 'One time password for verification is' + otp
+	}, function(error, message) {
+	    // The HTTP request to Twilio will run asynchronously. This callback
+	    // function will be called when a response is received from Twilio
+	    // The "error" variable will contain error information, if any.
+	    // If the request was successful, this value will be "falsy"
+	    if (!error) {
+		// The second argument to the callback will contain the information
+		// sent back by Twilio for the request. In this case, it is the
+		// information about the text messsage you just sent:
+		console.log('Success! The SID for this SMS message is:');
+		console.log(message.sid);
+
+		console.log('Message sent on:');
+		console.log(message.dateCreated);
+	    } else {
+		console.log('Oops! There was an error.');
+	    }
+	});
     
     response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
