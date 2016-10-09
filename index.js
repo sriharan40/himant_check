4bd5db809372f69de95ea84c396c437a0442d170
sriharan40 <sriharan40@gmail.com>
2016-10-09 23:02:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ if(action == "validateOTP")
 	var otp = data.result.parameters.inputOTP;
 	//var name = data.contexts.parameters.customerName;
 	var speech = data.result.fulfillment.speech + '.You entered :' + otp + '';
-
+	data.result.parameters.number = otp;
     response.statusCode = 200;
 	
     response.setHeader('Content-Type', 'application/json');	
@@ -117,6 +117,7 @@ if(action == "validateOTP")
      var responseBody = {
         "speech": speech,
         "displayText": speech,
+	"data": data,
         "source": "apiai-Himant-OTP sample"
     };
 	  
