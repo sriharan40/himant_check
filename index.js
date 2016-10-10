a9027b71a743cad8406ea79ce4d984eee3c9fdc8
sriharan40 <sriharan40@gmail.com>
2016-10-10 08:55:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ if(action == "validateOTP")
 {
 	var otp = data.result.parameters.inputOTP;
 
-	var otp_check = data.contexts[1].parameters.number;	
+	var otp_check = data.contexts.parameters.number;	
 
 	if(otp == otp_check)
 	{
