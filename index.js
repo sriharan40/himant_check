22786d142dff35809a65802e360565e14d8a5b31
sriharan40 <sriharan40@gmail.com>
2016-10-17 01:35:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -166,21 +166,7 @@ if(action == "getOutstandingBalance")
      var responseBody = {
         "speech": speech,
         "displayText": speech,
-	"data": {"facebook": "message":{
-    "text":"Pick a color:",
-    "quick_replies":[
-      {
-        "content_type":"text",
-        "title":"Red",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-      },
-      {
-        "content_type":"text",
-        "title":"Green",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-      }
-    ]
-  }},
+	"data": {"facebook": },
         "source": "apiai-Himant-OTP sample"
     };
 }
