34929360638d5dd0d4899ecd12f3b665af440cec
sriharan40 <sriharan40@gmail.com>
2016-11-13 19:54:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var request = require("request");
 var util = require("util");
 var http = require('http');
 var apiai = require("apiai");
-var dashbot = require('./dashbot')(process.env.DASHBOT_API_KEY,
+var dashbot = require('dashbot')(process.env.DASHBOT_API_KEY,
   {debug:true, urlRoot: process.env.DASHBOT_URL_ROOT}).facebook;
 var api = apiai(process.env.APIAI_ACCESS_TOKEN);
 	
