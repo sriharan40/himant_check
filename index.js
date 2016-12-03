8ce0790ad7c3673c19543f9fb9fbf7f32a6f5cbc
sriharan40 <sriharan40@gmail.com>
2016-12-03 16:28:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -580,8 +580,10 @@ dashbot.logOutgoing(requestData, response.body);
 	}
 	
 }
+    
     response.setHeader('Content-Type', 'application/json');	
     response.write(JSON.stringify(responseBody));
+    console.log ("Response is :" + JSON.stringify(responseBody));
     response.end();
 
 });  
