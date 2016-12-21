dc89b4e0d5ce6f5d6338415ae6051110df11e381
sriharan40 <sriharan40@gmail.com>
2016-12-21 16:01:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -501,7 +501,7 @@ if(action == "validateOTP")
 	var otp1 = data.result.parameters.inputOTP;
 
 	//var otp_check1 = data.result.contexts[2].parameters.number;
-	var ContextArray = data.result.contexts;
+	var contextArray = data.result.contexts;
 	console.log ("Contexts array :" + contextArray);
 	var sender = data.result.contexts[0].parameters.facebook_user_id;
 
